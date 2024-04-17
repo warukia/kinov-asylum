738cdc557d3931f3827cd57cbc753377391b8e5b
Warukia <aliciaperezkrasnoperova@gmail.com>
2024-04-17 14:02:38 +0200

    ummmmm

---
+++ Kinov Asylum/Assets/[2] Scripts/PlayerDeathSceneController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathSceneController : MonoBehaviour
+{
+    // Este script del diablo calcula qué escena de muerte aparecerá dependiendo de la entidad
+    // que mate a Yuliya.
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
